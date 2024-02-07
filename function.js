e086b06a70152018053f8d9b52ea010e9b4248d6
Idba1 <moniraislamself@gmail.com>
2024-02-07 07:21:44 +0600

    splice & function

---
+++ function.js
@@ -0,0 +1,7 @@
+function sum(a,b){
+    let totalsum = sum;
+    sum = a+b;
+    return totalsum;
+}
+sum(13,14);
+console.log(sum);
---
+++ splice.js
@@ -0,0 +1,2 @@
+const numbers = [33, 55, 8, 11, 56, 77, 32, 22, 86, 96];
+console.log(numbers.splice(2,5, 100,200));
