f115821f8e8e8e4293b338701e367fe416ff3ba7
Idba1 <moniraislamself@gmail.com>
2024-02-07 07:10:39 +0600

    nested-if-else.js

---
+++ nested-if-else.js
@@ -0,0 +1,12 @@
+let hour = 18;
+if(hour>=6 && hour<12){
+    console.log("good morning");
+}
+else{
+    if(hour>=12 && hour<18){
+        console.log("good afternoon");
+    }
+    else{
+        console.log("good evening");
+    }
+}
